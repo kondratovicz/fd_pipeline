13c04eb5940e5901ca863cc8a9c7ddf6000e7576
Pavel <ko@ko.ko>
2024-04-01 12:13:30 +0300

    added html template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Ma Nishma?</h1>'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
